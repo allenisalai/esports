99e6820df871a53aff629a9c374f4475780a3cf3
Allen Lai <allen.m.lai@gmail.com>
2017-11-02 20:02:36 -0700

    render the bare bone output form the details file

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import json
+import io
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    #thawed = jsonpickle.decode(read('/hots_output/Tomb_of_the_Spider_Queen/details.json'))
+    file = open('/hots_output/Tomb_of_the_Spider_Queen/details.json')
+
+    details = json.loads(file.read())
+    #for player in details['m_playerList']
+#        print(player['m_name'])
+
+    return json.dumps(details['m_playerList'])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9090)
+    app.run(debug=true, host='0.0.0.0', port=9090)
